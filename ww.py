62816335006c3c7a0abe8b178afd1b06a8e2b76a
waallf <30369148+waallf@users.noreply.github.com>
2018-08-30 19:40:54 +0800

    Update ww.py

--- ww.py
+++ ww.py
@@ -3,7 +3,9 @@ HP = int(input().split( )[0])
 normal = float(input().split( )[0])
 buffed = float(input().split( )[0])
 
-if buffed >=2*normal:
+if buffed > HP:
+    print(2)
+if buffed >= 2*normal:
     if HP % buffed ==0:
         print(int((HP / buffed)  * 2 ))
 
