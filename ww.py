a64d70a2d514f25207abf3d88814f9b85045efe9
waallf <30369148+waallf@users.noreply.github.com>
2018-08-30 20:33:59 +0800

    Update ww.py

--- ww.py
+++ ww.py
@@ -1,21 +1,41 @@
-import math
-HP = int(input().split( )[0])
-normal = float(input().split( )[0])
-buffed = float(input().split( )[0])
+n, l = [int(a) for a in input().strip().split()]
+chars = [[] for _ in range(l)]
+words = []
+for _ in range(n):
+    tmp_word = input()
+    words.append(tmp_word)
+    for i, x in enumerate(chars):
+        x.append(tmp_word[i])
+words = list(set(words))
+words.sort()
+n = len(words)
+for i, _ in enumerate(chars):
+    chars[i] = list(set(chars[i]))
+    chars[i].sort()
+gen_words_index = [0 for i in range(l)]
 
-if normal >= HP:
-    print(1)
-elif buffed >= HP:
-    print(2)
-elif buffed >= 2*normal:
-    if HP % buffed ==0:
-        print(int((HP / buffed)  * 2 ))
+i = l - 1
+all_index = 0
+# print(words)
+# print(chars)
 
-    else:
-        if HP % buffed >=  normal:
-            print(math.ceil((HP / buffed) * 2))
-        else:
-            print ((math.ceil(HP / buffed) -1)*2 +1)
+while all_index < n:
+    curr_word = ''.join([chars[i][gen_words_index[i]] for i in range(l)])
+    ### print('curr', i, gen_words_index)
+    ### print(curr_word, words[all_index])
+    if curr_word != words[all_index]:
+        print(curr_word)
+        exit()
+    all_index += 1
+    while i >= 0 and gen_words_index[i] >= len(chars[i]) - 1:
+        i -= 1
+    if i < 0:
+        break
+    gen_words_index[i] += 1
+    i += 1
+    while i <= l - 1:
+        gen_words_index[i] = 0
+        i += 1
+    i = l - 1
 
-else:
-    print(math.ceil(HP / normal))
+print('-')
